0d997913a23f0139f14ed0ffd5dee5937143c9c3
Gabriel <gabriel.fonseca@mjv.com.br>
2019-05-13 16:39:08 -0300

    Update

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CursosComponent } from './cursos.component';
 import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
 import { CursosService } from './cursos.service';
-import { ProvidersFeature } from '@angular/core/src/render3';
 
 @NgModule({
   declarations: [CursosComponent, CursosDetalheComponent],
