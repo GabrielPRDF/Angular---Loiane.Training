e7b00c846ffb8017524f7d92ba9a2560cdcd257d
Gabriel <gabriel.fonseca@mjv.com.br>
2019-06-24 13:46:29 -0300

    Input-Property

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { InputPropertyComponent } from './input-property/input-property.component';
 import { MeuFormModule } from './meu-form/meu-form.module';
 
 @NgModule({
-  declarations: [AppComponent, DataBindingComponent],
+  declarations: [AppComponent, DataBindingComponent, InputPropertyComponent],
   imports: [BrowserModule, FormsModule, MeuFormModule],
   providers: [],
   bootstrap: [AppComponent]
